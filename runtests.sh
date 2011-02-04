3161fbeb3bd644daefc6c274ad71f7e5340c8b14
Chris Reuter <chris@blit.ca>
2011-02-04 09:01:17 -0500

    Regression tests now working (sort of)

--- runtests.sh
+++ runtests.sh
@@ -2,5 +2,45 @@
 
 set -e
 
-#for i in test*.dk; do  ../udeck.pl $i > ${i%.dk}.txt; done
-#for i in fail*.dk; do  ../udeck.pl $i > ${i%.dk}.txt 2>&1 ; done
+tmp=/tmp/runtests-$$.txt
+
+cd tests
+
+function fail() {
+#	echo "Test failed.  Quitting."
+	rm $tmp
+	exit 1
+}
+
+
+for i in test*.dk; do
+	echo "$i"
+	../udeck.pl $i > $tmp
+	if diff $tmp ${i%.dk}.txt; then
+		true
+	else
+		fail
+	fi
+done
+
+
+for i in fail*.dk; do
+	echo $i
+	if ../udeck.pl $i > $tmp 2>&1 ; then
+		echo "$i succeeded.  Should have died."
+		echo "Output:"
+		cat $tmp
+		fail
+	fi
+
+	if diff $tmp ${i%.dk}.txt; then
+		true
+	else
+		fail
+	fi
+done
+
+
+
+
+
