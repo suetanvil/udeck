34f4ff00bf660d5c7dec3085132b7092b3ff3ce2
Chris Reuter <chris@blit.ca>
2011-04-14 09:12:02 -0400

    Turned 'isIndexable' into an attribute.

--- udeck.pl
+++ udeck.pl
@@ -3131,7 +3131,6 @@ sub initGlobals {
 	};
 
   defclass 'Number',		'Object', {};
-  defclass 'String',		'Object', {};
   defclass 'List',			'Object',
 	{at			=> sub {my ($self, $index) = @_; checkNargs(\@_, 2);
 						return $self->at($index)},
@@ -3140,6 +3139,20 @@ sub initGlobals {
 	 size_get	=> sub {my ($self) = @_; checkNargs(\@_, 2);
 						return decktype($self->size());},
 	};
+
+
+  defclass 'Stringlike',	'Object',
+	{at			=> sub {my ($self, $index) = @_; checkNargs(\@_, 2);
+						return $self->at($index)},
+	 atPut		=> sub {my ($self, $index, $value) = @_; checkNargs(\@_, 3);
+						return $self->atPut($index, $value)},
+	 size_get	=> sub {my ($self) = @_; checkNargs(\@_, 2);
+						return decktype($self->size());},
+	};
+
+  defclass 'String',		'Stringlike', {};
+  defclass 'Symbol',		'Stringlike', {};
+  defclass 'ByteArray',		'Stringlike', {};
 
   defclass 'Nil',			'Object', {};
   defclass 'Quote',			'Object', {};
