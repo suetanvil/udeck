28118f63794edf5de16c0d6757d8c449c2902acf
Chris Reuter <chris@blit.ca>
2011-08-13 15:55:57 -0400

    Added libref.

--- docs/format.sh
+++ docs/format.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+set -e
+
+if ../udeck.pl ../progs/utils/print_docstrings.dk > ds.tmp; then
+	mv ds.tmp libref-body.pod
+else
+	echo "Error creating libref-body.pod.  Using old version."
+fi
+
+cat libref-head.pod libref-body.pod | \
+    pod2html --css format.css --no-index \
+    --title "Deck Library Reference" > libref.html
+
 pod2html --css format.css --noindex intro.pod > intro.html
